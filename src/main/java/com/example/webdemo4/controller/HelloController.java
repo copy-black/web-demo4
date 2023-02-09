4cad680404c8152b46f53dc5e0552aaa4d5e3aa2
ZHENG ZHIJUN <1848892406@qq.com>
2023-02-09 09:56:33 +0800

    init

--- src/main/java/com/example/webdemo4/controller/HelloController.java
+++ src/main/java/com/example/webdemo4/controller/HelloController.java
@@ -1,2 +1,13 @@
-package com.example.webdemo4.controller;public class HelloController {
+package com.example.webdemo4.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
--- src/test/java/com/example/webdemo4/WebDemo4ApplicationTests.java
+++ src/test/java/com/example/webdemo4/WebDemo4ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebDemo4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
